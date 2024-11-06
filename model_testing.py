b1f761e93b361941442c96e5e1270e33564086a7
dsotten <146957918+dsotten@users.noreply.github.com>
2024-11-06 17:47:13 -0500

    Change .cuda() if necessary

--- model_testing.py
+++ model_testing.py
@@ -1,11 +1,19 @@
+import ast
 import pandas as pd
-from transformers import AutoTokenizer, AutoModelForCausalLM
+import extract_and_mask_if2
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, TFAutoModel, LlamaTokenizer, T5ForConditionalGeneration, T5Tokenizer
+import torch
+import numpy
+# import tensorflow as tf
 
-model_path = "./finetuned_model" #path/to/your/model/or/name/on/hub
-# device = "cpu" # or "cuda" if you have a GPU
+# model_path = "./finetuned_model/model.safetensors" #path/to/your/model/or/name/on/hub
+model_path = "./finetuned_model"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
-model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto')
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = T5ForConditionalGeneration.from_pretrained(model_path, device_map=device)
+# tokenizer = T5Tokenizer.from_pretrained(model_path)
+tokenizer = LlamaTokenizer.from_pretrained("huggyllama/llama-7b")
 
 # inputs = tokenizer.encode("This movie was really", return_tensors="pt")
 # outputs = model.generate(inputs)
@@ -13,22 +21,64 @@ tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 generated_test_csv = 'finetune_test_dataset.csv'
 provided_test_csv = 'sample.csv'
-generated_test_df = pd.DataFrame(generated_test_csv)
-provided_test_df = pd.DataFrame(provided_test_csv)
+generated_test_df = pd.read_csv(generated_test_csv)
+provided_test_df = pd.read_csv(provided_test_csv)
+
+generated_test_df['input_ids'] = generated_test_df['input_ids'].apply(ast.literal_eval)
+generated_test_df['target_block'] = generated_test_df['target_block'].apply(ast.literal_eval)
 
 generated_testset_results = []
 provided_testset_results = []
+
 num_preds = 0
 correct_preds = 0
 
-for _, row in generated_test_df.iterrows():
-    model_input = tokenizer.encode(row['input_method'])
+print("Beginning sample dataset testing")
+
+for _, row in provided_test_df.iterrows():
+    # model_input = tokenizer.encode(row['input_method'])
+    model_input = tokenizer(row['input_method'], return_tensors="pt", truncation=True, max_length=2048).input_ids
     expected_if = row['target_block']
     pred_correct = False
     num_preds += 1
 
     #How do we actually call the model?
-    predicted_if = model.generate(model_input)
+    predicted_if = model.generate(model_input.cuda())
+    # pred_str = ''
+    predicted_if = tokenizer.decode(predicted_if[0], skip_special_tokens=True)
+    # predicted_if = tokenizer.decode(predicted_if[0])
+    # for i in predicted_if:
+    #         pred_str += tokenizer.decode(predicted_if[i], skip_special_tokens=True)
+
+    if (expected_if == predicted_if):
+        pred_correct = True
+        correct_preds += 1
+
+    provided_testset_results.append({
+        'model_input': row['input_method'],
+        'pred_correct': pred_correct,
+        'expected_if': expected_if,
+        'predicted_if': predicted_if,
+        'pred_score': correct_preds/num_preds
+    })
+pd.DataFrame(provided_testset_results).to_csv('provided-testset.csv', index=False)
+
+num_preds = 0
+correct_preds = 0
+
+print("Beginning generated dataset testing")
+
+for _, row in generated_test_df.iterrows():
+    # model_input = tokenizer(row['input_ids'], return_tensors="pt", truncation=True, max_length=2048)
+    # model_input = row['input_ids']
+    model_input = torch.tensor(row['input_ids']).unsqueeze(0)
+    expected_if = row['target_block']
+    pred_correct = False
+    num_preds += 1
+
+    #How do we actually call the model?
+    predicted_if = model.generate(model_input.cuda())
+    predicted_if = tokenizer.decode(predicted_if[0], skip_special_tokens=True)
 
     if (expected_if == predicted_if):
         pred_correct = True
@@ -41,29 +91,7 @@ for _, row in generated_test_df.iterrows():
         'predicted_if': predicted_if,
         'pred_score': correct_preds/num_preds
     })
-pd.DataFrame(generated_testset_results).to_csv('generated-testset', index=False)
+pd.DataFrame(generated_testset_results).to_csv('generated-testset.csv', index=False)
 
-num_preds = 0
-correct_preds = 0
 
-for _, row in provided_test_df.iterrows():
-    model_input = tokenizer.encode(row['input_method'])
-    expected_if = row['target_block']
-    pred_correct = False
-    num_preds += 1
 
-    #How do we actually call the model?
-    predicted_if = model.generate(model_input)
-
-    if (expected_if == predicted_if):
-        pred_correct = True
-        correct_preds += 1
-
-    provided_testset_results.append({
-        'model_input': model_input,
-        'pred_correct': pred_correct,
-        'expected_if': expected_if,
-        'predicted_if': predicted_if,
-        'pred_score': correct_preds/num_preds
-    })
-pd.DataFrame(provided_testset_results).to_csv('provided-testset', index=False)
